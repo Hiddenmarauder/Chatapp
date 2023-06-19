57860c3686c56a02685cee876401a8cb755b1fa0
Hiddenmarauder <horl6038@gmail.com>
2023-06-19 09:02:08 -0700

    V1.1.5

--- dist/script.js
+++ dist/script.js
@@ -1,48 +1,120 @@
+// JavaScript code goes here
+// You can add your logic and functionality in this script
 
-
-window.addEventListener('DOMContentLoaded', function() {
-    var messageInput = document.getElementById('message-input');
-    var sendButton = document.getElementById('send-button');
-    var chatMessages = document.getElementById('chat-messages');
-    var timeElement = document.getElementById('timestamp')
+// Wait for the DOM to load
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the input field and send button elements
+    var inputField = document.querySelector('.chat__conversation-panel__input');
+    var sendButton = document.querySelector('.send-message-button');
   
+    // Add event listener to the send button
     sendButton.addEventListener('click', function() {
-      var message = messageInput.value;
-      if (message.trim() !== '') {
-        var messageElement = document.createElement('message-body');
-        messageElement.classList.add('message');
+      // Get the input value
+      var message = inputField.value;
   
-        var senderElement = document.createElement('span');
-        senderElement.classList.add('sender');
-        senderElement.textContent = 'User:  '; // Replace with actual sender name
-        messageElement.appendChild(senderElement);
+      // Clear the input field
+      inputField.value = '';
   
-        var textElement = document.createElement('span');
-        textElement.textContent = message;
-        messageElement.appendChild(textElement);
+      // Create a new message container element
+      var messageContainer = document.createElement('div');
+      messageContainer.classList.add('chat__conversation-board__message-container');
   
-        var timeElement = document.createElement('time');
-        timeElement.classList.add('timestamp');
-        timeElement.textContent = getCurrentTime();
-        messageElement.appendChild(timeElement);
+      // Create the person element
+      var personElement = document.createElement('div');
+      personElement.classList.add('chat__conversation-board__message__person');
   
-        chatMessages.appendChild(messageElement);
-        messageInput.value = '';
-      }
+      // Create the avatar element
+      var avatarElement = document.createElement('div');
+      avatarElement.classList.add('chat__conversation-board__message__person__avatar');
+  
+      // Create the nickname element
+      var nicknameElement = document.createElement('span');
+      nicknameElement.classList.add('chat__conversation-board__message__person__nickname');
+
+      var randomAvatarNumber = Math.floor(Math.random() * 10);
+      avatarElement.style.backgroundImage = `url(https://randomuser.me/api/portraits/lego/${randomAvatarNumber}.jpg)`;
+  
+      // Create the context element
+      var contextElement = document.createElement('div');
+      contextElement.classList.add('chat__conversation-board__message__context');
+      nicknameElement.textContent = 'User';
+
+      // Create the bubble element
+      var bubbleElement = document.createElement('div');
+      bubbleElement.classList.add('chat__conversation-board__message__bubble');
+  
+      // Create the span element for the message text
+      var messageText = document.createElement('span');
+      messageText.textContent = message;
+  
+      // Append the message text to the bubble element
+      bubbleElement.appendChild(messageText);
+  
+      // Append the avatar element, nickname element, and bubble element to the person element
+      personElement.appendChild(avatarElement);
+      personElement.appendChild(nicknameElement);
+      personElement.appendChild(bubbleElement);
+  
+      // Append the person element and context element to the message container
+      messageContainer.appendChild(personElement);
+      messageContainer.appendChild(contextElement);
+  
+      // Append the message container to the conversation board
+      var conversationBoard = document.querySelector('.chat__conversation-board');
+      conversationBoard.appendChild(messageContainer);
+
+
     });
-  
-    messageInput.addEventListener('keydown', function(event) {
-      if (event.key === 'Enter') {
-        sendButton.click();
-      }
-    });
-  
-    function getCurrentTime() {
-      var now = new Date();
-      var hours = now.getHours();
-      var minutes = now.getMinutes();
-      var time = hours + ':' + (minutes < 10 ? '0' : '') + minutes;
-      return time;
-    }
   });
+
+
+        // Get the emoji button element
+var emojiButton = document.getElementById('emoji-button');
+
+// Add event listener to the emoji button
+emojiButton.addEventListener('click', function() {
+  // Open the emoji modal or popover
+  openEmojiModal();
+});
+
+// Function to open the emoji modal
+function openEmojiModal() {
+  // Show the emoji selection modal
+  // You can implement your own logic to display a modal or popover with emoji options
+  // For example, you can create a hidden div with a list of emojis and show it as a modal when the emoji button is clicked
+  var emojiModal = document.getElementById('emoji-modal');
+  emojiModal.style.display = 'block';
+
+  // Add event listener to the emoji options
+  var emojiOptions = emojiModal.getElementsByClassName('emoji-option');
+  for (var i = 0; i < emojiOptions.length; i++) {
+    emojiOptions[i].addEventListener('click', function() {
+      // Get the selected emoji
+      var selectedEmoji = this.textContent;
+
+      // Set the selected emoji as the value of the input field or use it in any other way you need
+      inputField.value += selectedEmoji;
+
+      // Close the emoji modal or popover
+      closeEmojiModal();
+    });
+  }
+}
+
+// Function to close the emoji modal
+function closeEmojiModal() {
+  // Hide the emoji selection modal
+  var emojiModal = document.getElementById('emoji-modal');
+  emojiModal.style.display = 'none';
+};
+
   
+  // Check if the user is signed in
+var isAuthenticated = checkUserAuthentication(); // Replace this with your own authentication check logic
+
+// Redirect based on authentication status
+if (isAuthenticated) {
+  window.location.href = 'chat.html'; // Redirect to the chat UI page
+} else {
+  window.location.href = 'signin.html'; // Redirect to the signin page
+};
